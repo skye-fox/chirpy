e87bfe6a51b099a18ef3c57fd0c97d8b221b7645
skye-fox <105951568+skye-fox@users.noreply.github.com>
2024-07-23 21:36:01 -0500

    Add new metrics & reset endpoints. Add new middleware for counting hits to homepage

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -11,9 +12,13 @@ func main() {
 		port         = "8080"
 	)
 
+	apiCFG := apiConfig{}
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/*", apiCFG.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCFG.handlerMetrics)
+	mux.HandleFunc("/reset", apiCFG.handlerReset)
 
 	server := &http.Server{
 		Addr:    "localhost:" + port,
@@ -24,8 +29,19 @@ func main() {
 	log.Fatal(server.ListenAndServe())
 }
 
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(http.StatusText(http.StatusOK)))
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	hits := fmt.Sprintf("Hits: %d", cfg.fileserverHits)
+	w.Header().Add("Content-Type", "text/plain charset=utf-8")
+	w.Write([]byte(hits))
 }
