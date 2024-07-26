8120c19d22a8b1d468f47116597b205a683d675f
skye-fox <105951568+skye-fox@users.noreply.github.com>
2024-07-26 15:45:36 -0500

    Add endpoints for GET & POST requests at api/chirps. Move logic out of main into separate files

--- main.go
+++ main.go
@@ -1,20 +1,30 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
-	"slices"
-	"strings"
+
+	"github.com/skye-fox/chirpy/internal/database"
 )
+
+type apiConfig struct {
+	db             database.DB
+	fileserverHits int
+}
 
 func main() {
 	const (
 		filepathRoot = "."
 		port         = "8080"
+		dbPath       = "database.json"
 	)
 
+	chirpDB, err := database.NewDB(dbPath)
+	if err != nil {
+		log.Fatal(err)
+	}
 	apiCFG := apiConfig{
+		db:             *chirpDB,
 		fileserverHits: 0,
 	}
 
@@ -24,7 +34,8 @@ func main() {
 
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 	mux.HandleFunc("GET /api/reset", apiCFG.handlerReset)
-	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirps)
+	mux.HandleFunc("POST /api/chirps", apiCFG.handlerPostChirps)
+	mux.HandleFunc("GET /api/chirps", apiCFG.handlerGetChirps)
 
 	mux.HandleFunc("GET /admin/metrics", apiCFG.handlerMetrics)
 
@@ -35,74 +46,4 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
-}
-
-type apiConfig struct {
-	fileserverHits int
-}
-
-func handlerValidateChirps(w http.ResponseWriter, r *http.Request) {
-	type parameters struct {
-		Body string `json:"body"`
-	}
-
-	type returnVals struct {
-		Cleaned_body string `json:"cleaned_body"`
-	}
-
-	decoder := json.NewDecoder(r.Body)
-	params := parameters{}
-	err := decoder.Decode(&params)
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters")
-		return
-	}
-
-	const maxChirpLength = 140
-	if len(params.Body) > maxChirpLength {
-		respondWithError(w, http.StatusBadRequest, "Chirp is too long")
-		return
-	}
-
-	cleanBody := cleanBody(params.Body)
-
-	respondWithJSON(w, http.StatusOK, returnVals{
-		Cleaned_body: cleanBody,
-	})
-}
-
-func respondWithError(w http.ResponseWriter, code int, msg string) {
-	if code > 499 {
-		log.Printf("Responding with 5xx error: %s", msg)
-	}
-	type errorResponse struct {
-		Error string `json:"error"`
-	}
-	respondWithJSON(w, code, errorResponse{
-		Error: msg,
-	})
-}
-
-func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	w.Header().Set("Content-Type", "application/json")
-	data, err := json.Marshal(payload)
-	if err != nil {
-		log.Printf("Error marshalling JSON: %s", err)
-		w.WriteHeader(500)
-		return
-	}
-	w.WriteHeader(code)
-	w.Write(data)
-}
-
-func cleanBody(body string) string {
-	const censored = "****"
-	bannedWords := []string{"kerfuffle", "sharbert", "fornax"}
-	words := strings.Split(body, " ")
-	for i, word := range words {
-		if slices.Contains(bannedWords, strings.ToLower(word)) {
-			words[i] = censored
-		}
-	}
-	return strings.Join(words, " ")
 }
