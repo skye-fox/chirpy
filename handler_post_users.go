177d6a2d199d7dbf87fb748ca8e69bf64e8c9359
skye-fox <105951568+skye-fox@users.noreply.github.com>
2024-07-28 13:27:56 -0500

    Modify handler to accept & hash passwords before adding to db & use function to check for duplicates

--- handler_post_users.go
+++ handler_post_users.go
@@ -3,11 +3,19 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/skye-fox/chirpy/internal/auth"
 )
+
+type User struct {
+	Id    int    `json:"id"`
+	Email string `json:"email"`
+}
 
 func (cfg *apiConfig) handlerPostUsers(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
-		Email string `json:"email"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -18,11 +26,21 @@ func (cfg *apiConfig) handlerPostUsers(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	user, err := cfg.db.CreateUser(params.Email)
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't create user")
+	hashedPassword, err := auth.HashPassword(params.Password)
+
+	if !cfg.db.CheckDuplicateEmail(params.Email) {
+		user, err := cfg.db.CreateUser(params.Email, hashedPassword)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Couldn't create user")
+			return
+		}
+
+		respondWithJSON(w, http.StatusCreated, User{
+			Id:    user.Id,
+			Email: user.Email,
+		})
+		return
 	}
 
-	respondWithJSON(w, http.StatusCreated, user)
-
+	respondWithError(w, http.StatusOK, "Account already exists")
 }
