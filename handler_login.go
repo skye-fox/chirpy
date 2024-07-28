5d75a5dc21422be95e7879b47d8705d6e1f3eb0f
skye-fox <105951568+skye-fox@users.noreply.github.com>
2024-07-28 13:28:25 -0500

    Add handlerLogin function to handle requests to login endpoint

---
+++ handler_login.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/skye-fox/chirpy/internal/auth"
+)
+
+func (cfg *apiConfig) handlerLogin(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters")
+		return
+	}
+
+	user, err := cfg.db.GetUserByEmail(params.Email)
+	if err != nil {
+		respondWithError(w, http.StatusOK, "Couldn't find user")
+		return
+	}
+
+	err = auth.CheckPasswordHash(params.Password, user.HashedPassword)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Invalid password")
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, User{
+		Id:    user.Id,
+		Email: user.Email,
+	})
+}
