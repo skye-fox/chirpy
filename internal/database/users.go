d3e25894b94bac65404a89f36a21c159d4a1e602
skye-fox <105951568+skye-fox@users.noreply.github.com>
2024-08-03 14:06:28 -0500

    Add function GetUserByID & Remove refresh tokens from User struct

--- internal/database/users.go
+++ internal/database/users.go
@@ -1,7 +1,6 @@
 package database
 
 import (
-	"errors"
 	"log"
 )
 
@@ -9,7 +8,6 @@ type User struct {
 	Id             int    `json:"id"`
 	Email          string `json:"email"`
 	HashedPassword string `json:"hashed_password"`
-	RefreshToken   string `json:"refresh_token"`
 }
 
 func (db *DB) CreateUser(email, hashedPassword string) (User, error) {
@@ -43,7 +41,7 @@ func (db *DB) UpdateUsers(id int, email, hashedPassword string) (User, error) {
 
 	user, ok := appDB.Users[id]
 	if !ok {
-		return User{}, errors.New("User not found")
+		return User{}, ErrNotExist
 	}
 
 	user.HashedPassword = hashedPassword
@@ -85,27 +83,19 @@ func (db *DB) GetUserByEmail(email string) (User, error) {
 		}
 	}
 
-	return User{}, errors.New("User not found")
+	return User{}, ErrNotExist
 }
 
-func (db *DB) AddRefreshToken(refreshToken string, id int) error {
+func (db *DB) GetUserByID(id int) (User, error) {
 	appDB, err := db.loadDB()
 	if err != nil {
-		return err
+		return User{}, err
 	}
 
 	user, ok := appDB.Users[id]
 	if !ok {
-		return errors.New("User not found")
+		return User{}, ErrNotExist
 	}
 
-	user.RefreshToken = refreshToken
-	appDB.Users[id] = user
-
-	err = db.writeDB(appDB)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return user, nil
 }
